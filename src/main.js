06a7e9444e549161af6e9fec6edd3ac7dc81e54e
erfan <erfanm502@gmail.com>
2023-07-31 04:54:14 +0330

    onclick changed to addEventListener

--- src/main.js
+++ src/main.js
@@ -78,3 +78,33 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
 /******/ })()
 ;
 
+let intervalId;
+
+function add() {
+    let seconds = document.querySelector("#seconds").innerHTML;
+    let tens = document.querySelector("#tens").innerHTML;
+    let newSeconds,newTens;
+    if (parseInt(tens) === 59) {
+        seconds = (parseInt(seconds)+1).toString();
+        newSeconds = '0'+seconds;
+        document.querySelector("#seconds").innerHTML = (seconds<10) ? newSeconds : seconds ;
+        document.querySelector("#tens").innerHTML = "00";
+    } else {
+        tens = (parseInt(tens)+1).toString();
+        newTens = '0'+tens;
+        document.querySelector("#tens").innerHTML = (tens<10) ? newTens : tens ;
+    }
+}
+
+document.querySelector("#button-start").addEventListener("click",function() {
+    intervalId = setInterval(add,100);
+})
+
+document.querySelector("#button-stop").addEventListener("click",function() {
+    clearInterval(intervalId);
+})
+
+document.querySelector("#button-reset").addEventListener("click",function() {
+    document.querySelector("#seconds").innerHTML = "00";
+    document.querySelector("#tens").innerHTML = "00";
+})
